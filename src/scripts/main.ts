f898019df6f2c6f0938f16fb3442c65fab9ec7be
sincerajn <sincerajn@outlook.com>
2026-05-21 15:07:50 +0800

    refactor: inline copy button logic into component

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,11 +1,9 @@
 import { setupLangSwitch } from './i18n'
 import { setupTabs } from './tabs'
-import { setupCopyButtons } from './copier'
 import { setupReveal } from './revealer'
 
 document.addEventListener('DOMContentLoaded', () => {
   setupLangSwitch()
   setupTabs()
-  setupCopyButtons()
   setupReveal()
 })
